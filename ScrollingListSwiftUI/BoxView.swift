3c5e5b61a81e162af6a9ec35b211605dcf4de1eb
Anirup Patnaik <anirup049@gmail.com>
2019-11-23 21:09:27 +0530

    first commit

---
+++ ScrollingListSwiftUI/BoxView.swift
@@ -0,0 +1,21 @@
+//
+//  BoxView.swift
+//  ScrollingListSwiftUI
+//
+//  Created by Anirup Patnaik on 23/11/19.
+//  Copyright © 2019 Virtuelabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct BoxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BoxView_Previews: PreviewProvider {
+    static var previews: some View {
+        BoxView()
+    }
+}
